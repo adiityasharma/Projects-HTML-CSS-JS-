53bffc715833168ed87984abaac9556e3823d5f6
Aditya Sharma <adityasharma626367@gmail.com>
2025-04-18 20:38:43 +0530

    updated

--- cynthiaugwuWebClone/script.js
+++ cynthiaugwuWebClone/script.js
@@ -11,3 +11,46 @@ function movingCircle(){
   })
 }
 movingCircle()
+
+
+gsap.from(".heading h1",{
+  x: "200px",
+  opacity: 0,
+  duration: 1.5,
+  delay: 1
+})
+
+gsap.from(".heading h2",{
+  x: "-200px",
+  opacity: 0,
+  duration: 1.5,
+  delay: 1
+})
+
+gsap.from(".heading h5",{
+  x: "200px",
+  opacity: 0,
+  duration: 2,
+  delay: 1
+})
+
+gsap.from("nav h1, nav h2",{
+  y: "20px",
+  opacity: 0,
+  duration: 1,
+  delay: .5
+})
+gsap.from(".elem h1, .elem p",{
+  y: "40px",
+  opacity: 0,
+  duration: 1,
+  delay: .7,
+  scrollTrigger: {
+    trigger: ".second",
+    scroller:  "body",
+    markers: true,
+    start: "top 60%",
+    end: "top 30%",
+  },
+  stagger: .4
+})
